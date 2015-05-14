50c242903f95ed91d98637ce17f4329b51dd07ca
KimByongMo <kbm0818@naver.com>
2015-05-14 22:41:09 +0900

    방만들기.쪽지보내기

--- OCSF/src/old/Client.java
+++ OCSF/src/old/Client.java
@@ -16,15 +16,15 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class Client extends JFrame implements ActionListener{
+public class Client extends JFrame implements ActionListener {
 
-	
 	// Login GUI
 	private JFrame Login_GUI = new JFrame();
 	private JPanel LoginPane;
@@ -32,7 +32,7 @@ public class Client extends JFrame implements ActionListener{
 	private JTextField port_tf;
 	private JTextField id_tf;
 	private JButton login_btn = new JButton("Login");
-	
+
 	// Main GUI
 	private JPanel contentPane;
 	private JTextField message_tf;
@@ -48,148 +48,181 @@ public class Client extends JFrame implements ActionListener{
 	private Socket socket;
 	private String ip;
 	private int port;
-	private String id="";
+	private String id = "";
 	private InputStream is;
 	private OutputStream os;
 	private DataInputStream dis;
 	private DataOutputStream dos;
-	
-	Vector user_list=new Vector();
-	Vector room_list=new Vector();
+
+	// 유저목록 방목록 관련 변수
+	Vector user_list = new Vector();
+	Vector room_list = new Vector();
 	StringTokenizer st;
-	
-	Client()
-	{
+
+	private String My_Room;
+
+	Client() {
 		Login_init(); // login screen
 		Main_init(); // main screen
 		start();
 	}
-	
-	private void Main_init()
-	{
+
+	private void Main_init() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 700, 555);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("join room");
 		lblNewLabel.setBounds(12, 10, 80, 15);
 		contentPane.add(lblNewLabel);
-		
-		
+
 		notesend_btn.setBounds(12, 205, 125, 23);
 		contentPane.add(notesend_btn);
-		
-		
+
 		Room_list.setBounds(12, 269, 125, 160);
 		contentPane.add(Room_list);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("create room");
 		lblNewLabel_1.setBounds(12, 244, 80, 15);
 		contentPane.add(lblNewLabel_1);
-		
-		
+
 		User_list.setBounds(12, 35, 125, 160);
 		contentPane.add(User_list);
-		
+
 		joinroom_btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		joinroom_btn.setBounds(12, 439, 125, 23);
 		contentPane.add(joinroom_btn);
-		
-		
+
 		createroom_btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		createroom_btn.setBounds(12, 472, 125, 23);
 		contentPane.add(createroom_btn);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(180, 10, 492, 440);
 		contentPane.add(scrollPane);
-		
-		
+
 		scrollPane.setViewportView(Chat_area);
-		
+
 		message_tf = new JTextField();
 		message_tf.setBounds(180, 472, 375, 22);
 		contentPane.add(message_tf);
 		message_tf.setColumns(10);
-		
-		
+
 		send_btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		send_btn.setBounds(575, 472, 97, 23);
 		contentPane.add(send_btn);
-		
 		this.setVisible(true);
 	}
-	
-	private void start()
+
+	private void inmessage(String str) // 서버로부터 들어오는 모든 메세지
 	{
+		st = new StringTokenizer(str, "/");
+
+		String protocol = st.nextToken();
+		String Message = st.nextToken();
+
+		System.out.println("프로토콜 :" + protocol);
+		System.out.println("내용:" + Message);
+
+		if (protocol.equals("NewUser")) {
+			user_list.add(Message);
+			User_list.setListData(user_list);
+
+			User_list.updateUI();
+
+		} else if (protocol.equals("OldUser")) {
+			user_list.add(Message);
+			User_list.setListData(user_list);
+		} else if (protocol.equals("Note")) {
+			st = new StringTokenizer(Message, "@");
+			String user = st.nextToken();
+			String note = st.nextToken();
+
+			System.out.println(user + "사용자부터 받은 메세지 " + note);
+
+			JOptionPane.showMessageDialog(null, note, user + "님으로 부터 쪽지",
+					JOptionPane.CLOSED_OPTION);
+		} else if (protocol.equals("CreateRoom")) {
+			My_Room = Message;
+		} else if (protocol.equals("CreateRoomFail")) {
+			JOptionPane.showMessageDialog(null, "방 만들기 실패", "알림",
+					JOptionPane.CLOSED_OPTION);
+		} else if (protocol.equals("New_Room")) {
+			room_list.add(Message);
+			Room_list.setListData(room_list);
+		} else if (protocol.equals("Chatting")) {
+			String msg = st.nextToken();
+
+			Chat_area.append(Message + ":" + msg + "\n");
+		}
+
+	}
+
+	private void start() {
 		login_btn.addActionListener(this); //
 		notesend_btn.addActionListener(this); //
 		joinroom_btn.addActionListener(this); //
 		createroom_btn.addActionListener(this); //
 		send_btn.addActionListener(this); //
 	}
-	
-	private void Login_init()
-	{
+
+	private void Login_init() {
 		Login_GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Login_GUI.setBounds(100, 100, 300, 350);
 		LoginPane = new JPanel();
 		LoginPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		Login_GUI.setContentPane(LoginPane);
 		LoginPane.setLayout(null);
-		
+
 		JLabel lblServerIp = new JLabel("Server IP");
 		lblServerIp.setBounds(12, 65, 57, 15);
 		LoginPane.add(lblServerIp);
-		
+
 		ip_tf = new JTextField();
 		ip_tf.setBounds(81, 62, 116, 21);
 		LoginPane.add(ip_tf);
 		ip_tf.setColumns(10);
-		
+
 		JLabel lblSeverPort = new JLabel("Sever port");
 		lblSeverPort.setBounds(12, 110, 57, 15);
 		LoginPane.add(lblSeverPort);
-		
+
 		port_tf = new JTextField();
 		port_tf.setBounds(81, 107, 116, 21);
 		LoginPane.add(port_tf);
 		port_tf.setColumns(10);
-		
+
 		JLabel lblId = new JLabel("ID");
 		lblId.setBounds(12, 160, 57, 15);
 		LoginPane.add(lblId);
-		
+
 		id_tf = new JTextField();
 		id_tf.setBounds(81, 157, 116, 21);
 		LoginPane.add(id_tf);
 		id_tf.setColumns(10);
-		
-		
+
 		login_btn.setBounds(81, 230, 116, 23);
 		LoginPane.add(login_btn);
-		
+
 		Login_GUI.setVisible(true);
 	}
-	
-	private void Network()
-	{
+
+	private void Network() {
 		try {
-			socket=new Socket(ip, port);
-			if(socket!=null)
-			{
+			socket = new Socket(ip, port);
+			if (socket != null) {
 				Connection();
 			}
 		} catch (UnknownHostException e) {
@@ -198,66 +231,44 @@ public class Client extends JFrame implements ActionListener{
 			e.printStackTrace();
 		}
 	}
-	
-	private void Connection()
-	{
-		try	{
-			is=socket.getInputStream();
-			dis=new DataInputStream(is);
-			
-			os=socket.getOutputStream();
-			dos=new DataOutputStream(os);
+
+	private void Connection() {
+		try {
+			is = socket.getInputStream();
+			dis = new DataInputStream(is);
+
+			os = socket.getOutputStream();
+			dos = new DataOutputStream(os);
+		} catch (IOException e) {
+
 		}
-		catch(IOException e)
-		{
-			
-		}
-		
+
 		Send_message(id);
-		
-		//add user list at lobby
+
+		// User_list에 사용자 추가
 		user_list.add(id);
 		User_list.setListData(user_list);
-		
-		Thread th=new Thread(new Runnable()	{
-			
+
+		Thread th = new Thread(new Runnable() {
+
 			@Override
 			public void run() {
-				while(true)
-				{
+				while (true) {
 					try {
-						String msg=dis.readUTF();
-						System.out.println("message from server : "+msg);
+						String msg = dis.readUTF();
+						System.out.println("message from server : " + msg);
 						inmessage(msg);
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-					
+
 				}
 			}
-			
+
 		});
-		
 		th.start();
 	}
-	
-	private void inmessage(String str) // message from server
-	{
-		st=new StringTokenizer(str, "/");
-		
-		String protocal=st.nextToken();
-		String Message=st.nextToken();
 
-		System.out.println("protocal : "+protocal);
-		System.out.println("message : "+Message);
-		
-		if(protocal.equals("NewUser"))
-		{
-			user_list.add(Message);
-			User_list.setListData(user_list);
-		}
-	}
-	
 	private void Send_message(String str) // send message to server
 	{
 		try {
@@ -266,7 +277,7 @@ public class Client extends JFrame implements ActionListener{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 
 		new Client();
@@ -274,30 +285,35 @@ public class Client extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		if(e.getSource()==login_btn)
-		{
+
+		if (e.getSource() == login_btn) {
 			System.out.println("logint button click");
-			ip=ip_tf.getText().trim();
-			port=Integer.parseInt(port_tf.getText().trim());
-			id=id_tf.getText().trim();
+			ip = ip_tf.getText().trim();
+			port = Integer.parseInt(port_tf.getText().trim());
+			id = id_tf.getText().trim();
 			Network();
-		}
-		else if(e.getSource()==notesend_btn)
-		{
+		} else if (e.getSource() == notesend_btn) { // 쪽지보내기
 			System.out.println("note send button click");
-		}
-		else if(e.getSource()==joinroom_btn)
-		{
+			String user = (String) User_list.getSelectedValue(); // 유저 이름획득
+
+			String note = JOptionPane.showInputDialog("보낼메시지"); // 팝업창띄우고
+
+			if (note != null) {
+				Send_message("Note/" + user + "@" + note);
+			}
+			System.out.println("받는사람:" + user + " 보낼 내용:" + note);
+		} else if (e.getSource() == joinroom_btn) {
 			System.out.println("join room button click");
-		}
-		else if(e.getSource()==createroom_btn)
-		{
+		} else if (e.getSource() == createroom_btn) {
+			String roomname = JOptionPane.showInputDialog("방 이름");
+			if (roomname != null) {
+				Send_message("CreateRoom/" + roomname);
+			}
 			System.out.println("create room button click");
-		}
-		else if(e.getSource()==send_btn)
-		{
-			Send_message("test");
+		} else if (e.getSource() == send_btn) {
+			Send_message("Chatting/" + My_Room + "/"
+					+ message_tf.getText().trim());
+			// 프로토콜 + 방이름 + 메시지
 			System.out.println("send button click");
 		}
 	}
--- OCSF/src/old/Server.java
+++ OCSF/src/old/Server.java
@@ -19,33 +19,30 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class Server extends JFrame implements ActionListener{
+public class Server extends JFrame implements ActionListener {
 
-	
 	private JPanel contentPane;
 	private JTextField port_tf;
 	private JTextArea textArea = new JTextArea();
 	private JButton start_btn = new JButton("start");
 	private JButton stop_btn = new JButton("stop");
-	
+
 	private ServerSocket server_socket;
 	private Socket socket;
 	private int port;
-	private Vector user_vc=new Vector();
-	
-	Server()
-	{
-		
-		init(); // 
+	private Vector user_vc = new Vector();
+
+	Server() {
+
+		init(); //
 		start(); // ActionListener
 	}
-	
-	private void start()
-	{
+
+	private void start() {
 		start_btn.addActionListener(this);
 		stop_btn.addActionListener(this);
 	}
-	
+
 	public void init() // login gui
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -54,161 +51,147 @@ public class Server extends JFrame implements ActionListener{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(12, 10, 260, 200);
 		contentPane.add(scrollPane);
-		
-		
+
 		scrollPane.setViewportView(textArea);
-		
+
 		JLabel label = new JLabel("port");
 		label.setBounds(12, 220, 57, 15);
 		contentPane.add(label);
-		
+
 		port_tf = new JTextField();
 		port_tf.setBounds(81, 220, 191, 21);
 		contentPane.add(port_tf);
 		port_tf.setColumns(10);
-		
-		
+
 		start_btn.setBounds(12, 251, 120, 23);
 		contentPane.add(start_btn);
-		
-		
+
 		stop_btn.setBounds(152, 251, 120, 23);
 		contentPane.add(stop_btn);
-		
+
 		this.setVisible(true);
 	}
-	
-	
-	private void Server_start()
-	{
+
+	private void Server_start() {
 		try {
-			server_socket=new ServerSocket(port);
+			server_socket = new ServerSocket(port);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		if(server_socket!=null)
-		{
+
+		if (server_socket != null) {
 			Connection();
 		}
 	}
-	
-	private void Connection()
-	{
-		Thread th=new Thread(new Runnable() {
-			
+
+	private void Connection() {
+		Thread th = new Thread(new Runnable() {
+
 			@Override
 			public void run() {
-				while(true)	{
+				while (true) {
 					try {
 						textArea.append("waiting user connection\n");
-						socket=server_socket.accept();
+						socket = server_socket.accept();
 						textArea.append("connection success!!!!\n");
-						
-						UserInfo user=new UserInfo(socket);
+
+						UserInfo user = new UserInfo(socket);
 						user.start();
-						
+
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
 			}
 		});
-		
+
 		th.start();
 	}
-	
+
 	public static void main(String[] args) {
 
 		new Server();
 	}
 
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		if(e.getSource()==start_btn){
+
+		if (e.getSource() == start_btn) {
 			System.out.println("start btn click");
 			port = Integer.parseInt(port_tf.getText().trim());
 			Server_start();
-		}
-		else if(e.getSource()==stop_btn){
+		} else if (e.getSource() == stop_btn) {
 			System.out.println("stop btn click");
 		}
 	}
-	
-	class UserInfo extends Thread
-	{
+
+	class UserInfo extends Thread {
 		private OutputStream os;
 		private InputStream is;
 		private DataOutputStream dos;
 		private DataInputStream dis;
-		
+
 		private Socket user_socket;
-		private String Nickname="";
-		
-		UserInfo(Socket soc)
-		{
-			this.user_socket=soc;
+		private String Nickname = "";
+
+		UserInfo(Socket soc) {
+			this.user_socket = soc;
 			UserNetwork();
 		}
-		
-		private void UserNetwork()
-		{
-			try
-			{
-				is=user_socket.getInputStream();
-				dis=new DataInputStream(is);
-			
-				os=user_socket.getOutputStream();
-				dos=new DataOutputStream(os);
 
-				Nickname=dis.readUTF();
-				textArea.append(Nickname+" : user login\n");
-				
-				for(int i=0 ; i<user_vc.size(); i++) //send message to user that new user login
+		private void UserNetwork() {
+			try {
+				is = user_socket.getInputStream();
+				dis = new DataInputStream(is);
+
+				os = user_socket.getOutputStream();
+				dos = new DataOutputStream(os);
+
+				Nickname = dis.readUTF();
+				textArea.append(Nickname + " : user login\n");
+
+				for (int i = 0; i < user_vc.size(); i++) // send message to user
+															// that new user
+															// login
 				{
-					UserInfo u=(UserInfo)user_vc.elementAt(i);
-					u.send_Message("NewUser/"+Nickname);
+					UserInfo u = (UserInfo) user_vc.elementAt(i);
+					u.send_Message("NewUser/" + Nickname);
 				}
-				
-				//view user list
+
+				// view user list
 				user_vc.add(this); // then add to vector
+			} catch (IOException e) {
+
 			}
-			catch(IOException e)
-			{
-				
-			}
-			
+
 		}
-		
-		public void run() //do in thread
+
+		public void run() // do in thread
 		{
-			while(true)
-			{
+			while (true) {
 				try {
-					String msg=dis.readUTF();
-					textArea.append("message from "+Nickname+" : "+msg+"\n");
+					String msg = dis.readUTF();
+					textArea.append("message from " + Nickname + " : " + msg
+							+ "\n");
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-				
+
 			}
 		}
-		
-		private void send_Message(String str)
-		{
+
+		private void send_Message(String str) {
 			try {
 				dos.writeUTF(str);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
 }
